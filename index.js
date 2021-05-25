e88e1910ff3ddfc317292279ef4351903967a714
dino9611 <aldinorahman36@gmail.com>
2021-05-25 09:52:42 +0700

    bla

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const mysql = require("mysql");
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  port: 8081,
+  user: "root",
+  password: "password",
+  database: "hokibento",
+});
 
 app.get("/", (req, res) => {
   res.send("welcome docker images nodejs " + process.env.NAME);
+});
+
+const { promisify } = require("util");
+const db = promisify(conn.query).bind(conn);
+
+app.get("/user", async (req, res) => {
+  try {
+    const user = await db("select * from user");
+    return res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+    return res.send({ message: "error", error });
+  }
 });
 // console.log("dari docker " + process.env.NAME);
 app.listen(5000, () => console.log("app jalan di 5000 " + process.env.TEST));
